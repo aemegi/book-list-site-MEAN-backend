ea4f1cc7af644fd8eb1635aa5c450193694d4014
andrene1 <78675913+andrene1@users.noreply.github.com>
2023-08-24 14:25:47 -0700

    added routes to booksroute.js

--- api/BooksRoute.js
+++ api/BooksRoute.js
@@ -4,6 +4,8 @@ import ReviewsController from "./ReviewsController.js";
 export default class BooksRoute {
   static configRoutes(router) {
       router.route('/').get(BooksController.apiGetBooks);
+      router.route('/id/:id').get(BooksController.apiGetBookById);
+      router.route('/genre').get(BooksController.apiGetGenres);
 
       router
         .route('/review')
