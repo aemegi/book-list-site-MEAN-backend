4cfaf8cc3074f7ee8cf0a17f03008e092002277c
andrene1 <78675913+andrene1@users.noreply.github.com>
2023-08-24 13:54:14 -0700

    Update ReviewsDAO.js
    onto chapter 12 next

--- dao/ReviewsDAO.js
+++ dao/ReviewsDAO.js
@@ -15,4 +15,47 @@ export default class ReviewsDAO {
             console.error('unable to establish connection handle in reviewDao: ${e}');
         }
     }
+
+    static async addReview(bookId, user, review, date) {
+        try {
+            const reviewDoc = {
+                name: user.name,
+                user_id: user._id,
+                date,
+                review,
+                book_id: ReviewsDAO.ObjectId(bookId),
+            };
+            return await ReviewsDAO.reviews.insertOne(reviewDoc); 
+        } catch (e) {
+            console.error('unable to post review: ${e}');
+            return { error: e };
+        }
+    }
+
+    static async updateReview(reviewId,userId, review, date) {
+        try {
+            const updateResponse = await ReviewsDAO.reviews.updateOne(
+                { user_id: userId, _id: ReviewsDAO.ObjectId(reviewId) },
+                { $set: {review, date } },
+            );
+            return updateResponse;
+        } catch (e) {
+            console.error('unable to update review: ${e}');
+            return { error: e };
+        }
+    }
+
+    static async deleteReview(reivewId, userId) {
+        try {
+            const deleteResponse = await ReviewsDAO.reviews.deleteOne({
+                _id: ReviewsDAO.ObjectId(reviewId),
+                user_id: userId,
+            });
+            return deleteResponse;
+        } catch (e) {
+            console.error('unable to delete review: ${e}');
+            return { error: e };
+        }
+    }
+
 }
